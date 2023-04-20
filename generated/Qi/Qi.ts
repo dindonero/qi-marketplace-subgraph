d559d69493241305e8729211cb8431d3d2d51c1c
ChefMike <dinis.palha@gmail.com>
2023-04-20 13:14:01 +0100

    wrong abis fixed

--- generated/Qi/Qi.ts
+++ generated/Qi/Qi.ts
@@ -767,6 +767,32 @@ export class Qi extends ethereum.SmartContract {
   }
 }
 
+export class DefaultCall extends ethereum.Call {
+  get inputs(): DefaultCall__Inputs {
+    return new DefaultCall__Inputs(this);
+  }
+
+  get outputs(): DefaultCall__Outputs {
+    return new DefaultCall__Outputs(this);
+  }
+}
+
+export class DefaultCall__Inputs {
+  _call: DefaultCall;
+
+  constructor(call: DefaultCall) {
+    this._call = call;
+  }
+}
+
+export class DefaultCall__Outputs {
+  _call: DefaultCall;
+
+  constructor(call: DefaultCall) {
+    this._call = call;
+  }
+}
+
 export class AdminCall extends ethereum.Call {
   get inputs(): AdminCall__Inputs {
     return new AdminCall__Inputs(this);
